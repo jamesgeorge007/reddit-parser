4683a3f7b209f1686002caa23d1707309662db95
James George <jamesgeorge998001@gmail.com>
2018-07-12 19:31:36 +0530

    Project hierarchy setup :smiley:

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
-import RedditParser from './RedditParser';
 import './App.css';
+import RedditParser from './RedditParser';
 
 class App extends React.Component {
   public render() {
     return (
       <div>
+        <h1>Reddit Parser</h1>
         <RedditParser />
       </div>
     );
--- src/RedditParser.tsx
+++ src/RedditParser.tsx
@@ -1,9 +1,44 @@
 import * as React from 'react';
+import * as request from 'superagent';
 
-class RedditParser extends React.Component{
+interface defaultProps{
+  props: string
+};
+interface defaultState{
+  posts: <Array>string,
+  subreddit: string
+};
+
+class RedditParser extends React.Component<defaultProps, defaultState>{
+  constructor(props: any){
+    super(props);
+    this.state = {posts:[], subreddit: 'space'};
+    this.textInput = React.createRef();
+    this.getPosts = this.getPosts.bind(this);
+  }
+  public componentWillMount(){
+    this.getPosts();
+  }
+  public getPosts(){
+    request
+    .get(`https://www.reddit.com/r/#{this.state.subreddit}.json`)
+    .end((res, err) => {
+      if(err) {
+         // Handle error
+      }
+      const posts = res.data.data.children.map((item: any) => item.data);
+      this.setState(posts);
+    });
+  }
   public render(){
     return (
       <div>
+        <input type="text" ref={this.textInput}/>
+        <ul>
+          {this.state.posts.map((post) => {
+          <li key={post.id}>{post.title}</li>
+          })}
+        </ul>
       </div>
     );
   }
